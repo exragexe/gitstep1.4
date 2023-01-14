b9247bb2b2dc8173f16dec4f198281da39e7049c
yaros <yaroslavdisiak@gmail.com>
2023-01-14 15:36:11 +0200

    ex3doneHM

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int x,c,v,b,n,m,a;
+
+void Exercise() {
+    cout << "Enter 7 numbers: " << endl;
+    cin >> x;
+    cin >> c;
+    cin >> v;
+    cin >> b;
+    cin >> n;
+    cin >> m;
+    cin >> a;
+    int max = x;
+    if(max < c) max = c;
+    if(max < v) max = v;
+    if(max < b) max = b;
+    if(max < n) max = n;
+    if(max < m) max = m;
+    if(max < a) max = a;
+    cout << max;
+
+}
+int main()
+{
+    Exercise();
+    return 0;
+}
