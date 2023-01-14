6af49b6e1b9a8d269312787465d12969f93943ac
yaros <yaroslavdisiak@gmail.com>
2023-01-14 15:30:40 +0200

    ex2donePRACTICHNA

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
 
+
+#include <iostream>
+
+using namespace std;
+int x,y;
+
+void Exercise() {
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+    (x > y) ? cout << y << " is min" : cout << x << " is min";
+}
+
+int main()
+{
+    Exercise();
+    return 0;
+}
