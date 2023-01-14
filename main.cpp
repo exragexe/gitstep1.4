fe5545259f33df2e7f4642905e5885c288a1fe8d
yaros <yaroslavdisiak@gmail.com>
2023-01-14 15:33:25 +0200

    ex6donePRACTICHNA

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int x;
+
+void Exercise() {
+    cout << "Enter number: ";
+    cin >> x;
+
+    if (x % 2 ==0)
+    {
+        cout << x * 3;
+    }
+    else if (x % 2 !=0)
+    {
+        cout << x / 2;
+    }
+    else
+    {
+        cout << "Operation not found";
+    }
+
+}
+
+int main()
+{
+    Exercise();
+    return 0;
+}
