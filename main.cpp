18ff47febab8bfedf5e322a20497129827075430
yaros <yaroslavdisiak@gmail.com>
2023-01-14 15:31:14 +0200

    ex3donePRACTICHNA

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
 
+
+#include <iostream>
+
+using namespace std;
+int x;
+
+void Exercise() {
+    cout << "Enter number: ";
+    cin >> x;
+
+    if (x>0)
+    {
+        cout << x << " is positive";
+    }
+    else if (x<0)
+    {
+        cout << x << " is negative";
+    }
+    else
+    {
+        cout << x << " is equal to 0(zero)";
+    }
+}
+
+int main()
+{
+    Exercise();
+    return 0;
+}
