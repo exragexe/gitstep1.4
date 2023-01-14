94426cf9f515c16657edd2ba25916f0f67424a6c
yaros <yaroslavdisiak@gmail.com>
2023-01-14 15:29:58 +0200

    ex1donePRACTICHNA

--- main.cpp
+++ main.cpp
@@ -1,1 +1,25 @@
 
+
+#include <iostream>
+
+using namespace std;
+int a;
+
+void Exercise() {
+    cout << "Enter number: ";
+    cin >> a;
+    if (a %2 ==0)
+    {
+        cout << a <<" is even";
+    }
+    else
+    {
+        cout << a << " is odd";
+    }
+}
+
+int main()
+{
+    Exercise();
+    return 0;
+}
