ad3c5f4e1dec185c2104618686649c8e0b915407
yaros <yaroslavdisiak@gmail.com>
2023-01-14 15:32:03 +0200

    ex4donePRACTICHNA

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
 
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int x,y;
+
+void Exercise() {
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+
+    if (x==y)
+    {
+        cout <<"The numbers are equal";
+    }
+    else if (x!=y)
+    {
+        cout << min(x, y)<<" "<< max(x, y);
+    }
+    else
+    {
+        cout << x << " Operation not found";
+    }
+}
+
+int main()
+{
+    Exercise();
+    return 0;
+}
